9549fe80014231c67776961df1f0252c20fb3e41
shepiitkod <mopsolog@gmail.com>
2026-03-15 15:07:37 +0100

    Switch to Stripe Live and fix coupons

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+
+
 def cart(request):
     cart_data = request.session.get('cart', {})
     cart_items_count = sum(int(qty) for qty in cart_data.values())
     return {
         'cart_items_count': cart_items_count,
+        'STRIPE_PUBLISHABLE_KEY': settings.STRIPE_PUBLISHABLE_KEY,
     }
